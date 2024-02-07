a3689cb3c0552a5e8d106286c7be9edf2c78c11d
MilkyGee <milkcreame@gmail.com>
2024-02-06 21:08:08 -0800

    update

--- Assignments/mastermind.c
+++ Assignments/mastermind.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int main(void) // Main Scope
+{
+    
+    // Asks the user for game seed
+    unsigned int seed;
+    printf("Enter the integer value of the seed for the game: ");
+    scanf("%i",&seed);
+    srand(seed);
+
+
+}
