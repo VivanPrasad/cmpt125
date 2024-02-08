6ecd0c187db58f608e21f0de78f1d7aeba2115b4
MilkyGee <milkcreame@gmail.com>
2024-02-07 22:22:22 -0800

    test

--- Assignments/mastermind.c
+++ Assignments/mastermind.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ANSWER_DIGITS 6 // Number of digits in the sequence
+#define MIN_DIGIT 0 // Smallest possible digit to guess
+#define MAX_DIGIT 9 // Greatest possible digit to guess
+
+int get_seed(void) // Gets the initial seed
+{
+    unsigned int *seed = (unsigned int*)malloc(sizeof(unsigned int));
+    
+    // Ask user for seed (assuming case is always correct)
+    printf("Enter the integer value of the seed for the game: ");
+
+    scanf("%u",seed); // Read the input and sets the seed variable
+    srand(*seed); // Seed the pseudorandom number generator using seed
+    
+    free(seed); seed = NULL; // Free the dynamic seed variable
+    
+    return 0;
+}
+
+int play(void) // Play the game
+{ 
+    int answer[ANSWER_DIGITS];
+    for (int i; i < ANSWER_DIGITS; i++)
+    {
+        answer[i] = rand() % (MAX_DIGIT + 1 - MIN_DIGIT) + MIN_DIGIT; 
+        // Sets digits of answer from 0 to 9
+        printf("%u ",answer[i]);
+    }
+    return 0;
+}
+
 int main(void) // Main Scope
 {
-    
     // Asks the user for game seed
-    unsigned int seed;
-    printf("Enter the integer value of the seed for the game: ");
-    scanf("%i",&seed);
-    srand(seed);
-
-
+    get_seed();
+    play();
+    return 0;
 }
